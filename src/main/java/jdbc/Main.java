876adc2ab94ec42ad36eae016c419f262972dbab
Arnel <167875243+Arnel-rah@users.noreply.github.com>
2025-12-02 18:12:31 +0300

    Refactor: improve JDBC code using try-with-resources and remove unnecessary connection.close()

--- src/main/java/jdbc/Main.java
+++ src/main/java/jdbc/Main.java
@@ -1,16 +1,30 @@
 package jdbc;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args){
-        try (var connection =  DB.connect()){
-            System.out.println("Connected to the PostgreSQL database.");
+        String sql = "SELECT * FROM users ORDER BY id OFFSET 2";
+        try (
+                var connection = DB.connect()
+        ) {
+            assert connection != null;
+            try (var statement = connection.createStatement();
+                 var resultSet = statement.executeQuery(sql)
+                    ){
+                System.out.println("Connected to the PostgreSQL database.");
+
+                while (resultSet.next()) {
+                    System.out.println(resultSet.getString("name"));
+                }
+            }
         } catch (SQLException e) {
             System.err.println(e.getMessage());
         } finally {
-            
+            System.out.println("Disconnected from the PostgreSQL database.");
         }
     }
 }
